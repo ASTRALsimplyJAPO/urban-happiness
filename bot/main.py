70f75e532e6784c247c8bb3ece5ab681a0d3e876
ASTRALsimplyJAPO <117852294+ASTRALsimplyJAPO@users.noreply.github.com>
2022-11-28 18:40:57 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,18 @@
-import discord
-import os
-#import pynacl
-#import dnspython
-import server
+import discord, os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="?")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game('?help'))
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
 
-server.server()
+
 bot.run(TOKEN)
+
